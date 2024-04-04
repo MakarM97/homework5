8fd8f9b4097b63c827be6876475bd717185e45fd
MakarM97 <165716716+MakarM97@users.noreply.github.com>
2024-04-05 00:38:46 +1000

    Add files via upload

---
+++ Homework 5 Списки и словари.py
@@ -0,0 +1,13 @@
+food = ['apple', 'peach', 'banana' , 'orange' , 'kiwi', 'pear']
+print(food)
+ans = food[::len(food) - 1] #Попросиь объснить
+print(ans)
+print(food[3:5])
+food[3] = 'grape'
+print(food)
+#
+dictionary = {'apple': 'яблоко', 'peach': 'персик', 'banana': 'банан', 'grape': 'виноград'}
+print(dictionary)
+print(dictionary.get('grape'))
+dictionary['kiwi'] = 'киви'
+print(dictionary)
