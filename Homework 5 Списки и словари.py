53b9668f426ba77eac8a81d024a286b3683430ca
MakarM97 <165716716+MakarM97@users.noreply.github.com>
2024-04-06 20:06:47 +1000

    Add files via upload

--- Homework 5 Списки и словари.py
+++ Homework 5 Списки и словари.py
@@ -1,9 +1,11 @@
 food = ['apple', 'peach', 'banana' , 'orange' , 'kiwi', 'pear']
 print(food)
-ans = food[::len(food) - 1] #Попросиь объснить
-print(ans)
-print(food[2:4])
-food[3] = 'grape'
+first_element = food[0]
+last_element = food[-1]
+print(f'first_element: {first_element}')
+print(f'last_element: {last_element}')
+print(food[2:5])
+food[2] = 'grape'
 print(food)
 #
 dictionary = {'apple': 'яблоко', 'peach': 'персик', 'banana': 'банан', 'grape': 'виноград'}
